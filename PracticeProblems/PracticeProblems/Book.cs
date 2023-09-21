616dd99ad4d61583385e07ac2251b8d2f12f3671
BalikiriSudheer <sudheerbalikiri2000@gmail.com>
2023-09-21 19:17:57 +0530

    []Add : Demo of class and object.

---
+++ PracticeProblems/PracticeProblems/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class Book
+    {
+        public String title;
+        public String author;
+        public int pages;
+    }
+}
---
+++ PracticeProblems/PracticeProblems/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b=new Book();
+            b.title = "Harry potter";
+            b.author = "Rowling";
+            b.pages = 400;
+            Console.Write(b.title+" "+b.author+" "+b.pages);
+            Console.ReadLine();
+        }
+    }
+}
